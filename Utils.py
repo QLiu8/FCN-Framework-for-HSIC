dc15091bb062ce1ae5b78f7c77d880f68a3c4536
ChefLiu <liuqianahu@163.com>
2023-11-24 17:59:29 +0800

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -313,7 +313,7 @@ class Toolkits:
         if torch.cuda.is_available():
             torch.cuda.manual_seed(seed)
             torch.cuda.manual_seed_all(seed)  # if multi-GPU
-        # np.random.seed(seed)  # Numpy module.
+        np.random.seed(seed)  # Numpy module.
         random.seed(seed)  # Python random module.
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
